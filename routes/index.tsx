c402d24736551a59f42013539f2da5337f8f02c6
Luca Casonato <hello@lcas.dev>
2022-06-27 15:40:54 +0200

    Add post listing to index page
    The list of all published blog posts is now available on the index page.

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,11 +1,39 @@
 /** @jsx h */
 import { h } from "preact";
+import { Handlers, PageProps } from "$fresh/server.ts";
 import { tw } from "@twind";
+import { listPosts, Post } from "../utils/posts.ts";
 
-export default function Home() {
+export const handler: Handlers<Post[]> = {
+  async GET(_req, ctx) {
+    const posts = await listPosts();
+    return ctx.render(posts);
+  },
+};
+
+export default function Home(props: PageProps<Post[]>) {
+  const posts = props.data;
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      Hello World
+    <div class={tw`px-4 mx-auto max-w-screen-md`}>
+      <h1 class={tw`font-bold text-5xl mt-12`}>Luca's Blog</h1>
+      <ul class={tw`mt-8`}>
+        {posts.map((post) => <PostEntry post={post} />)}
+      </ul>
     </div>
+  );
+}
+
+function PostEntry(props: { post: Post }) {
+  const { post } = props;
+  return (
+    <li class={tw`border-t`}>
+      <a href={`/blog/${post.id}`} class={tw`py-2 flex group gap-4`}>
+        <div>{post.publishAt.toDateString()}</div>
+        <div>
+          <h2 class={tw`font-bold group-hover:underline`}>{post.title}</h2>
+          <p class={tw`text-gray-600`}>{post.snippet}</p>
+        </div>
+      </a>
+    </li>
   );
 }
