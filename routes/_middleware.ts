60cad78896552b6e5c888f25ceb4584649fcb883
Luca Casonato <hello@lcas.dev>
2022-06-27 16:50:36 +0200

    Update locale via POST request

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,18 +1,26 @@
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
 import { parse } from "accept-language-parser";
+import { getCookies } from "$std/http/cookie.ts";
 import { State } from "../utils/state.ts";
 
 export function handler(
   req: Request,
   ctx: MiddlewareHandlerContext<State>,
 ) {
-  const langs = parse(req.headers.get("accept-language") || undefined);
   ctx.state.locales = [];
+
+  const cookies = getCookies(req.headers);
+  if (cookies.locale) {
+    ctx.state.locales.push(cookies.locale);
+  }
+
+  const langs = parse(req.headers.get("accept-language") || undefined);
   for (const lang of langs) {
     let locale = lang.code;
     if (lang.region) locale += `-${lang.region}`;
     ctx.state.locales.push(locale);
   }
+
   if (ctx.state.locales.length === 0) ctx.state.locales.push("en-UK");
 
   return ctx.next();
--- routes/settings.tsx
+++ routes/settings.tsx
@@ -1,12 +1,25 @@
 /** @jsx h */
 import { h } from "preact";
 import { Handlers, PageProps } from "$fresh/server.ts";
+import { setCookie } from "$std/http/cookie.ts";
 import { tw } from "@twind";
 import { State } from "../utils/state.ts";
 
 export const handler: Handlers<State, State> = {
   GET(_req, ctx) {
     return ctx.render({ ...ctx.state });
+  },
+  async POST(req) {
+    const form = await req.formData();
+    const locale = form.get("locale");
+    const resp = new Response("", {
+      status: 303,
+      headers: { Location: "/settings" },
+    });
+    if (typeof locale === "string") {
+      setCookie(resp.headers, { name: "locale", value: locale });
+    }
+    return resp;
   },
 };
 
@@ -18,6 +31,21 @@ export default function SettingsPage(props: PageProps<State>) {
       <p class={tw`mt-4`}>
         Your current locale is <b>{locales[0]}</b>.
       </p>
+      <form action="/settings" method="post" class={tw`mt-4`}>
+        <label htmlFor="locale">Locale</label>
+        <input
+          type="text"
+          name="locale"
+          id="locale"
+          class={tw`border px-2 py-1`}
+        />
+        <button
+          type="submit"
+          class={tw`bg-blue(500 hover:700 disabled:200) text-white px-2 py-1`}
+        >
+          Save
+        </button>
+      </form>
     </div>
   );
 }
