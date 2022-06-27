385b6f85380342826e0ee53a7312b1abdd5d8f10
Luca Casonato <hello@lcas.dev>
2022-06-27 14:55:27 +0200

    Add loadPost helper function
    This function loads a blog post from disk given the post id.

--- utils/posts.ts
+++ utils/posts.ts
@@ -1,7 +1,31 @@
+import { extract } from "$std/encoding/front_matter.ts";
+
 export interface Post {
   id: string;
   title: string;
   publishAt: Date;
   snippet: string;
   content: string;
+}
+
+export async function loadPost(id: string): Promise<Post | null> {
+  let text: string;
+  try {
+    text = await Deno.readTextFile(`./data/posts/${id}.md`);
+  } catch (err) {
+    if (err instanceof Deno.errors.NotFound) {
+      return null;
+    }
+    throw err;
+  }
+  const { attrs, body } = extract(text);
+  const params = attrs as Record<string, string>;
+  const publishAt = new Date(params.publish_at);
+  return {
+    id,
+    title: params.title,
+    publishAt,
+    snippet: params.snippet,
+    content: body,
+  };
 }
